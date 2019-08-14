cee031dca96fd6ad34bb7b4747c1baa2d6928ded
Times125 <1251680944@qq.com>
2019-08-14 17:12:31 +0800

    break captcha version 1.0 beta

--- model.py
+++ model.py
@@ -42,7 +42,6 @@ def build_model():
     x = CNN5(inputs) if cnn_type == 'CNN5' else ResNet50(inputs)
     conv_shape = x.get_shape()
     x = Reshape(target_shape=(int(conv_shape[1]), int(conv_shape[2] * conv_shape[3])))(x)
-    # x = Reshape(target_shape=(int(conv_shape[1]), int(conv_shape[2] * conv_shape[3])))(x)
     # concat Bi-RNN layers to encode and decode sequence
     x = BiLSTM(x, use_gpu=config.use_gpu) if rnn_type == 'BiLSTM' else BiGRU(x, use_gpu=config.use_gpu)
     predictions = TimeDistributed(Dense(config.n_class, kernel_initializer='he_normal', activation='softmax'))(x)
@@ -61,46 +60,3 @@ def build_model():
     plot_model(base_model, './plotModel/{}-{}_base_model.png'.format(cnn_type, rnn_type), show_shapes=True)
     return model, base_model, int(conv_shape[1])
 
-
-def test_model():
-    inputs = Input((150, 50, 1))
-    x = inputs
-    for i in range(3):
-        x = Convolution2D(32 * 2 ** i, (3, 3), activation='relu', padding='same',
-                          kernel_regularizer=l2(0.01))(x)
-        # x = Convolution2D(32*2**i, (3, 3), activation='relu')(x)
-        x = BatchNormalization()(x)
-        x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Convolution2D(64, (3, 3), activation='relu', padding='same', kernel_regularizer=l2(0.01))(x)
-    # x = Convolution2D(32*2**i, (3, 3), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    conv_shape = x.get_shape()
-    print(conv_shape)
-    x = Reshape(target_shape=(int(conv_shape[1]) * int(conv_shape[2]), conv_shape[3]))(x)
-    x = Dense(32, activation='relu')(x)
-    '''加入gru'''
-
-    gru_1 = GRU(64, return_sequences=True, kernel_initializer='he_normal', name='gru1')(x)
-    gru_1b = GRU(64, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(x)
-    gru1_merged = add([gru_1, gru_1b])
-
-    gru_2 = GRU(64, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
-    gru_2b = GRU(64, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(
-        gru1_merged)
-    x = concatenate([gru_2, gru_2b])
-    x = Dropout(0.5)(x)
-    predictions = Dense(37, kernel_initializer='he_normal', activation='softmax')(x)
-    base_model = Model(inputs=inputs, outputs=predictions)
-
-    '''CTC_loss'''
-    labels = Input(name='the_labels', shape=[4, ], dtype='float32')
-    input_length = Input(name='input_length', shape=[1], dtype='int64')
-    label_length = Input(name='label_length', shape=[1], dtype='int64')
-    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')(
-        [predictions, labels, input_length, label_length])
-    model = Model(inputs=[inputs, labels, input_length, label_length], outputs=[loss_out])
-    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer='adam')
-    plot_model(base_model, 'theirs.png', show_shapes=True)
-    return model, base_model, int(conv_shape[1])
